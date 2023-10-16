3b2fff95eb045ce3b30efa696153080100eedc62
tara004 <singhtarnjot99@gmail.com>
2023-10-16 18:00:25 +0200

    aggiorno

---
+++ secondo_esercizio/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secondo_esercizio
+{
+    internal class Person
+    {
+        public string FirstName;
+        public string SecondName;
+        public string BirthCity;
+        public DateTime DateBirth;
+        public string Gender;
+        public string FiscalCode;
+
+        public Person(string firstName, string secondName, string birthCity, DateTime dateBirth, string gender)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+            BirthCity = birthCity;
+            DateBirth = dateBirth;
+            Gender = gender;
+            CodiceFiscaleUtility.CodiceFiscale cf;
+            cf = new CodiceFiscaleUtility.CodiceFiscale(secondName,firstName,gender,dateBirth,birthCity);
+        }
+        public void in_nome()
+        {
+            Console.WriteLine("Inserisci il nome: ");
+            FirstName = Console.ReadLine();
+        }
+        private void in_cognome()
+        {
+            Console.WriteLine("Inserisci il cognome: ");
+            SecondName = Console.ReadLine();
+        }
+        private void in_citta()
+        {
+            Console.WriteLine("Inserisci la città di nascita: ");
+            BirthCity = Console.ReadLine();
+        }
+        private void in_data()
+        {
+            Console.WriteLine("Inserisci la data di nascita: ");
+            //string.TryParse(Console.ReadLine(), out DateBirth);
+        }
+        private void genere()
+        {
+            Console.WriteLine("Inserisci il tuo genere: ");
+            Gender = Console.ReadLine();
+        }
+    }
+}
--- secondo_esercizio/Program.cs
+++ secondo_esercizio/Program.cs
@@ -1,3 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-//asf
+Console.WriteLine("Inserisci i dati della persona:");
+
+Person person = new Person();
+
+Console.Write("Nome: ");
+person.FirstName = Console.ReadLine();
+Console.Write("Cognome: ");
+person.SecondName = Console.ReadLine();
+Console.Write("Sesso: ");
+person.Gender = Console.ReadLine();
+Console.Write("Città di nascita: ");
+person.BirthCity = Console.ReadLine();
+Console.Write("Codice città di nascita: ");
+person.BirthCityCode = Console.ReadLine();
+Console.Write("Data di nascita (yyyy-MM-dd): ");
+if (DateTime.TryParse(Console.ReadLine(), out DateTime birthDate))
+{
+    person.BirthDate = birthDate;
+
+    Console.WriteLine($"Età: {person.Age()}");
+    Console.WriteLine($"Codice fiscale: {person.Fiscalcode()}");
+}
+else
+{
+    Console.WriteLine("Data di nascita non valida.");
+}
+class Person
+{
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+    public string Gender { get; set; }
+    public string BirthCity { get; set; }
+    public string BirthCityCode { get; set; }
+    public DateTime BirthDate { get; set; }
+
+    public int Age()
+    {
+        DateTime currentDate = DateTime.Now;
+        int age = currentDate.Year - BirthDate.Year;
+        if (BirthDate > currentDate.AddYears(-age))
+        {
+            age--;
+        }
+        return age;
+    }
+
+    public string Fiscalcode()
+    {
+        
+
+        return "XXXXXX";
+    }
+}
+
+
+
+
